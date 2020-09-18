da897e186f4e04464765e0363a5bfb7842315a0e
Alexander Sweet <alexnsweet@gmail.com>
2020-09-18 02:41:46 +0000

    reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	unsigned int rev_alf;
+
+	for (rev_alf = 122; rev_alf >= 97; rev_alf--)
+	{
+		putchar(rev_alf);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
