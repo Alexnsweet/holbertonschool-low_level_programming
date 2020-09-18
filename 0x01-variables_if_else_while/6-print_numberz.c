6ed7fe4bd5adbde8ff7cdc75da5fbb32328a84ed
Alexander Sweet <alexnsweet@gmail.com>
2020-09-18 02:36:53 +0000

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	unsigned int integer;
+
+	for (integer = 48; integer <= 57; integer++)
+	{
+		putchar(integer);
+	}
+
+	putchar('\n');
+
+	return (0);
+
+}
