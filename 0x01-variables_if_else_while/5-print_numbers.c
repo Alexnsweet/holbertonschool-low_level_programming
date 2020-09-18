c24d34f4f4a33cc5c56483fe104eef9ff76cfc95
Alexander Sweet <alexnsweet@gmail.com>
2020-09-18 02:35:57 +0000

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry Point
+ *
+ * Return: 0
+ */
+int main(void)
+{
+
+	int nums;
+
+	for (nums = 0; nums <= 9; nums++)
+	{
+		printf("%d", nums);
+	}
+
+	printf("\n");
+
+	return (0);
+
+}
