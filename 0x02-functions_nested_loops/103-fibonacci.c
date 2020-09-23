7d2733580a52cf5a7d0cb0c296bbdd53e57933c3
Alexander Sweet <alexnsweet@gmail.com>
2020-09-23 19:24:15 +0000

    I didn't change much

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	long num1, num2, fib, sum;
 
-	for (num1 = 1, num2 = 2, fib = 0, sum = 0; num1 < 4000000 || num2 < 4000000;)
+	for (num1 = 1, num2 = 2, fib = 0, sum = 2; num1 < 4000000 && num2 < 4000000;)
 	{
 	fib = num1 + num2;
 		if (fib % 2 == 0)
@@ -18,9 +18,11 @@ int main(void)
 	num1 = num2;
 	num2 = fib;
 
+
 	}
 
-printf("%ld", sum);
+	printf("%ld", sum);
+
 putchar('\n');
 
 return (0);
