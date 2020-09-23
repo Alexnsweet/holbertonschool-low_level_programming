0b3bb493df6074a962d608b7ba4bc1d716462a32
Alexander Sweet <alexnsweet@gmail.com>
2020-09-23 01:52:07 +0000

    conditional change

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,7 +8,7 @@ int main(void)
 {
 	long num1, num2, fib, sum;
 
-	for (num1 = 1, num2 = 2, fib = 0, sum = 0; num1 + num2 < 4000000;)
+	for (num1 = 1, num2 = 2, fib = 0, sum = 0; num1 < 4000000 && num2 < 4000000;)
 	{
 		if (fib % 2 == 0)
 		{
