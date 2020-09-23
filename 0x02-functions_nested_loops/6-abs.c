38cd560455208448ec6d9cea94d4a49920b8411d
Alexander Sweet <alexnsweet@gmail.com>
2020-09-23 01:35:52 +0000

    abs

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _abs - computes absolute value of a number
+ * @value: absolute value
+ *
+ * Return: Something
+ */
+int _abs(int value)
+{
+
+	if (value < 0)
+	{
+		value = value * -1;
+		return (value);
+	}
+
+		return (value);
+}
