2cbc7acd8e2d2739b977b576da91f30f806d393b
Alexander Sweet <alexnsweet@gmail.com>
2020-09-23 01:56:07 +0000

    changed order of defining fib before conditional logic

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -8,14 +8,13 @@ int main(void)
 {
 	long num1, num2, fib, sum;
 
-	for (num1 = 1, num2 = 2, fib = 0, sum = 0; num1 < 4000000 && num2 < 4000000;)
+	for (num1 = 1, num2 = 2, fib = 0, sum = 0; num1 < 4000000 || num2 < 4000000;)
 	{
+	fib = num1 + num2;
 		if (fib % 2 == 0)
 		{
 			sum = sum + fib;
 		}
-
-	fib = num1 + num2;
 	num1 = num2;
 	num2 = fib;
 
