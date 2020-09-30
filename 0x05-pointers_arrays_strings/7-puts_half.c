b5d4ed1a2ea217aef3e806aca0d55d70ed63185f
alexnsweet <alexnsweet@gmail.com>
2020-09-30 02:06:22 +0000

    put only half of the string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+/**
+ * puts_half - prints second half of string
+ * @str: string input
+ */
+void puts_half(char *str)
+{
+	long len = 0;
+	long n;
+
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+
+	if (len % 2 == 0)
+	{
+		for (n = len / 2; n < len; n++)
+		{
+			_putchar(str[n]);
+		}
+	}
+	else
+	{
+		for (n = (len -  1) / 2; n < len; ++n)
+		{
+			_putchar(str[n]);
+		}
+	}
+
+_putchar('\n');
+
+}
