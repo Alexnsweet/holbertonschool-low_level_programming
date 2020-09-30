f8a5d19ca69e2433b2c6d90e92057d937eb9a397
alexnsweet <alexnsweet@gmail.com>
2020-09-30 01:48:10 +0000

    puts

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,18 @@
+#include "holberton.h"
+/**
+ * _puts - similar to puts
+ * @str: string
+ *
+ */
+void _puts(char *str)
+{
+	int i;
+
+for (i = 0; str[i] != '\0'; i++)
+	{
+	_putchar(str[i]);
+	}
+_putchar('\n');
+
+
+}
