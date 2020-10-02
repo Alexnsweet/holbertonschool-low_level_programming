5bdf2259094b0106a55ad09c9f5cd2a09c615cc6
alexnsweet <alexnsweet@gmail.com>
2020-10-02 02:13:02 +0000

    added in null buffer at end

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -18,10 +18,13 @@ char *_strncpy(char *dest, char *src, int n)
 	while (dest[dest_len] != '\0')
 	{	dest_len++;	}
 
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{	dest[i] = src[i];	}
 
-	for (i = 0; i < n; i++)
+	while (i < n)
 	{
-		{	dest[i] = src[i];	}
+		dest[i] = '\0';
+		i++;
 	}
 
 	return (dest);
