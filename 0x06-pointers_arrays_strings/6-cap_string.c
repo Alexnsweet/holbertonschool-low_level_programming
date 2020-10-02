df2867c688f9280632dc7b2627a8938ca40e92a8
alexnsweet <alexnsweet@gmail.com>
2020-10-02 04:57:10 +0000

    ran out of time before deadline. Pick back up tomorrow

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * cap_string - capatalizes all words of a string.
+ *
+ * Return: character string
+ */
+char *cap_string(char *s)
+{
+	int i = 0;
+	char d[] = {' ', '	', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'}
+	while (s)
+	{
+		if s[i] == d
+
+
+
+
+
+}
