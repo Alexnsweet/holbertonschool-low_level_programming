f2a65a7f6c2c54a74dac306d3cd97cb2aeece5ac
alexnsweet <alexnsweet@gmail.com>
2020-10-02 01:58:06 +0000

    can't figure out the last print

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,29 @@
+#include "holberton.h"
+/**
+ * _strncpy - Copies a string
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to copy
+ *
+ * Return: dest
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int dest_len = 0, src_len = 0, i;
+
+	while (src[src_len] != '\0')
+	{	src_len++;	}
+
+	while (dest[dest_len] != '\0')
+	{	dest_len++;	}
+
+
+	for (i = 0; i < n; i++)
+	{
+		{	dest[i] = src[i];	}
+	}
+
+	return (dest);
+}
+
