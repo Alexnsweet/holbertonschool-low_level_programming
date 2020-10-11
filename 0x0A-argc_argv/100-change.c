f6bbba7b2d3cf7967a08eddc4b232ac0a2b4912b
alexnsweet <alexnsweet@gmail.com>
2020-10-11 23:40:41 +0000

    advanced

---
+++ 0x0A-argc_argv/100-change.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ * @argc: size
+ * @argv: string of
+ *
+ * Return: 1 if more than 1 argument, 0 if not or negative value
+ */
+int main(int argc, char argv[])
+{
+
+
+
+
+
+return (0);
+
+}
