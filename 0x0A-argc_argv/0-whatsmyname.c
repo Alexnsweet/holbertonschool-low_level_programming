b1bee8773d66e8a1116ef50ae19844dcc7be1c84
alexnsweet <alexnsweet@gmail.com>
2020-10-11 23:26:17 +0000

    whats my name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * main - Entry Point
+ * @argc: argument size
+ * @argv: string array of arguments
+ *
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+	{
+	printf("%s\n", *argv);
+	}
+return (0);
+}
