f6e702f54c487eecdeb552547e1a7d54e3875755
alexnsweet <alexnsweet@gmail.com>
2020-10-20 00:17:35 +0000

    added description

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,13 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - dog name age and owner
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner name of dog
+ *
+ * Long description of dog struct
+ */
 struct dog
 {
 	char *name;
