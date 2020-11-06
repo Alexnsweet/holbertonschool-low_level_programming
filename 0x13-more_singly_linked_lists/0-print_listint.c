207184c471acac1ed060315c5299c3529217ab9c
alexnsweet <alexnsweet@gmail.com>
2020-11-06 19:27:41 +0000

    print list

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+/**
+ * print_listint - prints elements and returns count
+ * @h: list
+ *
+ * Return: count
+ */
+
+size_t print_listint(const listint_t *h)
+{
+	int i;
+
+	while (h)
+	{	printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+return (i);
+}
