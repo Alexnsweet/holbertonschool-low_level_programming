75172bdc5de20f3eacac40719ce3c2335c95d6c8
Alexander Sweet <alexnsweet@gmail.com>
2020-12-11 14:38:24 +0000

    started

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - prints all the elements of a dlistint_t list
+ *
+ * @h: dlistint_t
+ *
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	
+
+
+
+
+}
