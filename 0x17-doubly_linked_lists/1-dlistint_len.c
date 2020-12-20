31cd6e83bef382ccf082f868e4860884d8839304
alexnsweet <alexnsweet@gmail.com>
2020-12-20 07:51:38 +0000

    returns number of elements

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,20 @@
+/**
+ * dlistint_len - returns the number of elements
+ *
+ * @h: dlist
+ *
+ * Return: number of elements in linked list
+ */
+
+size_t dlistint_len(const dlistint_t *h)
+{
+	size_t count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+	}
+
+	return (count);
+}
